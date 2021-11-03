9b5c8670f600152489b248fa939b8458e78e6562
Taly <vitalik7tv@yandex.ru>
2021-11-04 00:27:22 +0300

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -17,37 +17,37 @@ declare -a SOURCES=(
   "ramfjordmoen https://aurorainfo.eu/aurora-live-cameras/ramfjordmoen-norway-all-sky-aurora-live-camera.jpg"
   "sodankyla https://aurorainfo.eu/aurora-live-cameras/sodankyla-finland-all-sky-aurora-live-camera.jpg"
   "hankasalmi https://aurorainfo.eu/aurora-live-cameras/hankasalmi-finland-all-sky-aurora-live-camera.jpg"
-  "tesis https://tesis.lebedev.ru/upload_test/files/fc.png"
-  "swpc-solar-synoptic-map https://services.swpc.noaa.gov/images/synoptic-map.jpg"
-
-  "solar-0094 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0094.jpg"
-  "solar-0131 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0131.jpg"
-  "solar-0171 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0171.jpg"
-  "solar-0193 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0193.jpg"
-  "solar-0211 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0211.jpg"
-  "solar-0304 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg"
-  "solar-0335 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0335.jpg"
-  "solar-1600 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_1600.jpg"
-  "solar-1700 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_1700.jpg"
-  "solar-HMIIF https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIIF.jpg"
-  "solar-HMIBC https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIBC.jpg"
-  "solar-211193171 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_211193171.jpg"
-
-  "lasco-c2 https://services.swpc.noaa.gov/images/animations/lasco-c2/latest.jpg"
-  "lasco-c3 https://services.swpc.noaa.gov/images/animations/lasco-c3/latest.jpg"
-
-  "stereo-195 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_195_latest.jpg"
-  "stereo-171 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_171_latest.jpg"
-  "stereo-284 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_284_latest.jpg"
-  "stereo-304 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_304_latest.jpg"
-
-  "enlil https://services.swpc.noaa.gov/images/animations/enlil/latest.jpg"
-
-  "geo-density https://services.swpc.noaa.gov/images/animations/geospace/density/latest.png"
-  "geo-velocity https://services.swpc.noaa.gov/images/animations/geospace/velocity/latest.png"
-  "geo-pressure https://services.swpc.noaa.gov/images/animations/geospace/pressure/latest.png"
-  "geo-polar-lt https://services.swpc.noaa.gov/images/animations/geospace/polar_lt/latest.png"
-  "geo-global https://services.swpc.noaa.gov/images/animations/geospace/global/latest.png"
+#  "tesis https://tesis.lebedev.ru/upload_test/files/fc.png"
+#  "swpc-solar-synoptic-map https://services.swpc.noaa.gov/images/synoptic-map.jpg"
+#
+#  "solar-0094 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0094.jpg"
+#  "solar-0131 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0131.jpg"
+#  "solar-0171 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0171.jpg"
+#  "solar-0193 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0193.jpg"
+#  "solar-0211 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0211.jpg"
+#  "solar-0304 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg"
+#  "solar-0335 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0335.jpg"
+#  "solar-1600 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_1600.jpg"
+#  "solar-1700 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_1700.jpg"
+#  "solar-HMIIF https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIIF.jpg"
+#  "solar-HMIBC https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIBC.jpg"
+#  "solar-211193171 https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_211193171.jpg"
+#
+#  "lasco-c2 https://services.swpc.noaa.gov/images/animations/lasco-c2/latest.jpg"
+#  "lasco-c3 https://services.swpc.noaa.gov/images/animations/lasco-c3/latest.jpg"
+#
+#  "stereo-195 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_195_latest.jpg"
+#  "stereo-171 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_171_latest.jpg"
+#  "stereo-284 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_284_latest.jpg"
+#  "stereo-304 https://stereo.gsfc.nasa.gov/beacon/latest_256/ahead_euvi_304_latest.jpg"
+#
+#  "enlil https://services.swpc.noaa.gov/images/animations/enlil/latest.jpg"
+#
+#  "geo-density https://services.swpc.noaa.gov/images/animations/geospace/density/latest.png"
+#  "geo-velocity https://services.swpc.noaa.gov/images/animations/geospace/velocity/latest.png"
+#  "geo-pressure https://services.swpc.noaa.gov/images/animations/geospace/pressure/latest.png"
+#  "geo-polar-lt https://services.swpc.noaa.gov/images/animations/geospace/polar_lt/latest.png"
+#  "geo-global https://services.swpc.noaa.gov/images/animations/geospace/global/latest.png"
 )
 
 for i in "${SOURCES[@]}"
